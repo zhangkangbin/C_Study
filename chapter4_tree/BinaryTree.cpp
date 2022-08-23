d2333ab2e9dbf3f233814b6fd794112a58d5b0c0
zhangkangbin <784908058@qq.com>
2022-08-23 22:39:12 +0800

    为了方便参考，课本全部源码

--- chapter4_tree/BinaryTree.cpp
+++ chapter4_tree/BinaryTree.cpp
@@ -2,7 +2,7 @@
  * @Author: zhangkangbin 784908058@qq.com
  * @Date: 2022-08-23 13:58:39
  * @LastEditors: zhangkangbin 784908058@qq.com
- * @LastEditTime: 2022-08-23 16:54:55
+ * @LastEditTime: 2022-08-23 22:37:29
  * @FilePath: \C_Study\chapter4_tree\BinaryTree.cpp
  * 二叉树
  */
@@ -10,155 +10,208 @@
 #include <iostream>
 using namespace std;
 
-class TreeNode{
+class TreeNode
+{
 
-   public:
+public:
    int data;
    TreeNode *mLeftChild;
    TreeNode *mRightChild;
 };
 
-class BinaryTree{
+class BinaryTree
+{
 
-   public:
-   TreeNode *mRootNode=NULL;
-   TreeNode *mParentNode=NULL;
+public:
+   TreeNode *mRootNode = NULL;
+   TreeNode *mParentNode = NULL;
    void insertData(int value);
    void preOrder(TreeNode *node);
    void inOrder(TreeNode *node);
    void postOrder(TreeNode *node);
+   int countNode(TreeNode *node);
+   int countNode();
    void printfAllData();
-
 };
+/**
+ * @brief 统计结点个数。
+ *
+ * @return int
+ */
+int BinaryTree::countNode()
+{
+   return countNode(mRootNode);
+}
+int BinaryTree::countNode(TreeNode *node)
+{
+   if (node == NULL)
+   {
+
+      return 0;
+   }
+   else
+   {
+
+      //统计左结点。
+      int leftNode = countNode(node->mLeftChild);
+      //  cout<<"  \nleftNode:"<<leftNode<<"\n";
+      int rightNode = countNode(node->mRightChild);
+      // cout<<"  \nrightNode:"<<rightNode<<"\n";
+      int sum = leftNode + rightNode + 1; // 1为根节点
+
+      return sum;
+   }
+}
 /**
  * 递归方式
  * 先遍历
- * 
+ *
  */
-void BinaryTree::preOrder(TreeNode *node){
+void BinaryTree::preOrder(TreeNode *node)
+{
 
-   if(node!=NULL){
-     
-   //  cout<<"\nleft:"+1;
-     cout<<"\nleft:"<<node->data;
-     preOrder(node->mLeftChild);
-     preOrder(node->mRightChild);
-
-   }else{
-  //  cout<<"\n mRootNode is empty!";
-   } 
-
-/* 
-王道书上的代码应该是有问题的。
-//非递归方式需要一个父节点？。
-   TreeNode *tempNode=node;
-   
-   while (tempNode!=NULL)
-
+   if (node != NULL)
    {
-   
 
-       if(tempNode!=NULL){
-         //psuh(temp);指针进栈。
-
-         tempNode=tempNode->mLeftChild;
-
-       }else{
-            //pop(temp);指针退栈。
-           cout<<"\nleft:"<<node->data;
-           tempNode=tempNode->mRightChild;
-
-       }
+      //  cout<<"\nleft:"+1;
+      cout << "\nleft:" << node->data << "\n";
+      preOrder(node->mLeftChild);
+      preOrder(node->mRightChild);
    }
-    */
-   
+   else
+   {
+      //  cout<<"\n mRootNode is empty!";
+   }
+
+   /*
+   王道书上的代码应该是有问题的。
+   //非递归方式需要一个父节点？。
+      TreeNode *tempNode=node;
+
+      while (tempNode!=NULL)
+
+      {
+
+
+          if(tempNode!=NULL){
+            //psuh(temp);指针进栈。
+
+            tempNode=tempNode->mLeftChild;
+
+          }else{
+               //pop(temp);指针退栈。
+              cout<<"\nleft:"<<node->data;
+              tempNode=tempNode->mRightChild;
+
+          }
+      }
+       */
 }
 
-/** 
+/**
  *   中序
- * 
- * @param node 
+ *
+ * @param node
  */
-void BinaryTree::inOrder(TreeNode *node){
+void BinaryTree::inOrder(TreeNode *node)
+{
 
-   if(node!=NULL){
-     
+   if (node != NULL)
+   {
 
-     inOrder(node->mLeftChild);
-     //中序
-     cout<<"\nleft:"<<node->data;
-     inOrder(node->mRightChild);
-
-   }else{
-   // cout<<"\n mRootNode is empty!";
+      inOrder(node->mLeftChild);
+      //中序
+      cout << "\nleft:" << node->data;
+      inOrder(node->mRightChild);
    }
-
+   else
+   {
+      // cout<<"\n mRootNode is empty!";
+   }
 }
 
-
-/** 
+/**
  *   后序
- * 
- * @param node 
+ *
+ * @param node
  */
-void BinaryTree::postOrder(TreeNode *node){
+void BinaryTree::postOrder(TreeNode *node)
+{
 
-   if(node!=NULL){
-     
+   if (node != NULL)
+   {
 
-     postOrder(node->mLeftChild);
-    
-     postOrder(node->mRightChild);
+      postOrder(node->mLeftChild);
+
+      postOrder(node->mRightChild);
       //后序
-     cout<<"\nleft:"<<node->data;
-
-   }else{
-   // cout<<"\n mRootNode is empty!";
+      cout << "\nleft:" << node->data;
    }
-
+   else
+   {
+      // cout<<"\n mRootNode is empty!";
+   }
 }
-
 
 /**
  * todo:功能待完善。
- * 
- * @param value 
+ *
+ * @param value
  */
-void BinaryTree::insertData(int value){
+void BinaryTree::insertData(int value)
+{
 
-if(mRootNode==NULL){
-   mRootNode=new TreeNode();
-   mParentNode=mRootNode;
-}
-  TreeNode *node=new TreeNode();
-  node->data=value;
+   if (mRootNode == NULL)
+   {
+      mRootNode = new TreeNode();
+      mParentNode = mRootNode;
+   }
+   TreeNode *node = new TreeNode();
+   node->data = 1;
 
-  if(mParentNode->mLeftChild==NULL){
+   TreeNode *node2 = new TreeNode();
+   node2->data = 2;
 
-   mParentNode->mLeftChild=node;
-    return;
-  }
+   node->mLeftChild = node2;
 
-  if(mParentNode->mRightChild==NULL){
+   TreeNode *node3 = new TreeNode();
+   node3->data = 3;
 
-   mParentNode->mRightChild=node;
+   node->mRightChild = node3;
 
-   return;
-  }
+   mRootNode->mLeftChild = node;
 
-  //mParentNode=node;
+   TreeNode *nodeRight = new TreeNode();
+   nodeRight->data = 66;
+   mRootNode->mRightChild = nodeRight;
+
+   /*   if(mParentNode->mLeftChild==NULL){
+
+      mParentNode->mLeftChild=node;
+       return;
+     }
+
+     if(mParentNode->mRightChild==NULL){
+
+      mParentNode->mRightChild=node;
+
+      return;
+     } */
+
+   // mParentNode=node;
 }
 
 int main()
 {
- 
 
-  BinaryTree tree;
-  tree.insertData(1);
-  tree.insertData(2);
-  tree.preOrder(tree.mRootNode);
- // tree.inOrder(tree.mRootNode);
-//  tree.postOrder(tree.mRootNode);
-   
+   BinaryTree tree;
+   tree.insertData(1);
+   tree.preOrder(tree.mRootNode);
+   int sum = tree.countNode();
+   cout << "\n node count: " << sum << "\n";
+
+   // tree.inOrder(tree.mRootNode);
+   //  tree.postOrder(tree.mRootNode);
+
    return 0;
 }
