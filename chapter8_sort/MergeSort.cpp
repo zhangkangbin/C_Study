d3d62c6a9f620adb747113d39b01e7917afdf3dd
zhangkangbin <784908058@qq.com>
2022-09-23 23:16:49 +0800

    merge sort

---
+++ chapter8_sort/MergeSort.cpp
@@ -0,0 +1,24 @@
+/*
+ * @Author: zhangkangbin
+ * @Date: 2022-09-23 22:21:09
+ * @LastEditors: zhangkangbin
+ * @LastEditTime: 2022-09-23 23:16:03
+ * @FilePath: \C_Study\chapter8_sort\MergeSort.cpp
+ * 归并排序
+ */
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void mergeSort(){
+
+int list[7]={2,4,1,8,6,9,3};
+
+
+}
+int main()
+{
+    /* code */
+    cout<<(1/2);
+    return 0;
+}
