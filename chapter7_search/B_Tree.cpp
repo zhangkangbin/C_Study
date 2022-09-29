8a98f4e57b02742723ddd5aef55bfe5d3405b4fa
zhangkangbin <784908058@qq.com>
2022-09-29 17:00:27 +0800

    B Tree

--- chapter7_search/B_Tree.cpp
+++ chapter7_search/B_Tree.cpp
@@ -2,7 +2,7 @@
  * @Author: zhangkangbin
  * @Date: 2022-09-27 23:51:48
  * @LastEditors: zhangkangbin
- * @LastEditTime: 2022-09-28 18:56:39
+ * @LastEditTime: 2022-09-29 15:04:31
  * @FilePath: \C_Study\chapter7_search\B_Tree.cpp
  *
  *B+树。通常用于数据库和操作系统的文件系统中。这个不是重点
@@ -36,10 +36,44 @@ public:
     int i;         // 1..m，在结点中的关键字序号
     int tag;       // 1：查找成功，0：查找失败
 };
-BTreeNode *mRootTree=NULL;
+BTreeNode *mRootTree = NULL;
+/**
+ * ptr子树指针矢量
+ *
+ */
+void splitNode(BTreeNode *&tree, BTreeNode *&parent, int mid)
+{
 
+    int i;
+    parent = new BTreeNode();
+
+    for (i = mid + 1; i <= M; i++)
+    {
+        // i= 2, 3
+        cout << " \n ------------splitNode: i:" <<i<<"   "<< (i - mid - 1);
+        parent->key[i - mid - 1] = tree->key[i];
+        parent->ptr[i - mid - 1] = tree->ptr[i];
+    }
+
+    if (parent->ptr[0])
+    {
+        //当ap有子树的时候
+        for (i = 0; i <= 1; i++)
+        {
+
+            parent->ptr[i]->parent = parent;
+        }
+    }
+
+    parent->keyNum = (M - mid) - 1;
+    parent->parent = tree->parent;
+    
+    //修改tree的记录个数
+    tree->keyNum = tree->keyNum - (M - mid);
+}
 int seachIndex(BTreeNode *tree, int key)
 {
+    cout << " \n seachIndex : key:" << key;
     int endNum;
     if (tree)
     {
@@ -56,6 +90,11 @@ int seachIndex(BTreeNode *tree, int key)
         return i; //树存在，但仅有一个为空根节点
     }
 
+    if (key >= tree->key[endNum]) //节点不为空，但当前值比最大的key还大
+    {
+        return endNum;
+    }
+
     //节点不为空，但当前值比最小的key还小.
     if (key <= tree->key[1])
     {
@@ -63,10 +102,6 @@ int seachIndex(BTreeNode *tree, int key)
         return i;
     }
 
-    if (key <= tree->key[endNum]) //节点不为空，但当前值比最小的key还小
-    {
-        return i;
-    }
     //有合适的位置，即处于当前结点的最大和最小值之间，或找到了
     //在中间查找位置
     for (i = 1; i < endNum; i++)
@@ -81,27 +116,22 @@ int seachIndex(BTreeNode *tree, int key)
 }
 /**
  * 新建节点
- * 
+ *
  */
-void newRoot(BTreeNode*& tree,int key,BTreeNode*& ap){
+void newRoot(BTreeNode *&tree, int key, BTreeNode *&ap)
+{
 
-    BTreeNode* newTree=new BTreeNode();
-    newTree->key[1]=key;
-    newTree->ptr[0]=tree;
-    newTree->ptr[1]=ap;
-    newTree->keyNum=1;
-    newTree->parent=NULL;
+    BTreeNode *newTree = new BTreeNode();
+    newTree->key[1] = key;
+    newTree->ptr[0] = tree;
+    newTree->ptr[1] = ap;
+    newTree->keyNum = 1;
+    newTree->parent = NULL;
 
+    ap->parent = newTree;
+    tree->parent = newTree;
 
-    
-    ap->parent=newTree;
-    tree->parent=newTree;
-
-
-    tree=newTree;
-
-
-
+    tree = newTree;
 }
 Result searchBTree(int key)
 {
@@ -109,7 +139,7 @@ Result searchBTree(int key)
     bool isFound = false;
     int index = 0;
     BTreeNode *pNext = mRootTree;
-    BTreeNode *parent=NULL;
+    BTreeNode *parent = NULL;
     while (pNext && !isFound)
     {
         index = seachIndex(pNext, key);
@@ -167,13 +197,16 @@ void insert(BTreeNode *&tree, int keyIndex, int key)
 }
 
 /**
- * @brief 插入数据
+ *
+ *  插入数据
  *
  * @param key
+ * start function
  */
 void insertData(int key)
 {
-     //先查找到插入的位置。
+
+    //先查找到插入的位置。
     Result result = searchBTree(key);
     // 1：查找成功，0：查找失败。
     if (result.tag == TRUE)
@@ -181,13 +214,17 @@ void insertData(int key)
         //相同的数不重复插入。
         return;
     }
-    BTreeNode* ap=NULL;
+    int i = result.i;
+    int xKey = key;
+    BTreeNode *ap = NULL;
     bool isFinished = false;
     BTreeNode *next = result.pt;
     while (next && !isFinished)
     {
-        insert(next, result.i, key);
-
+        //插入数据
+        insert(next, i, xKey);
+        
+        //如果是否超过子树最大阶。
         if (next->keyNum < M)
         {
             isFinished = true;
@@ -195,34 +232,43 @@ void insertData(int key)
 
         else
         {
-            //超过最大个数，开始分裂
-            int mid=M/2;
+            //超过最大阶个数，开始分裂
+            int mid = M / 2;
 
+            splitNode(next, ap, mid);
 
-            
+            xKey = ap->key[0];
+            cout<<"\n key:"<<xKey;
+            next = next->parent;
+
+            if (next)
+            {
+
+                i = seachIndex(next, xKey);
+            }
         }
     }
     //是空树或者根结点已分裂为结点*q和*ap
-    //ap为子树指针
-    if(!isFinished){
-        newRoot(mRootTree,key,ap);
+    // ap为子树指针
+    if (!isFinished)
+    {
+        newRoot(mRootTree, xKey, ap);
     }
-   
 }
-void InitialBTree(BTreeNode* &T)
+void InitialBTree(BTreeNode *&T)
 {
-	//初始化一个空的根
-	T->keyNum=0;		
-	T->parent=NULL;	
-	for(int i=0;i<M+1;i++)
-	{
-		T->ptr[i]=NULL;
-	}
+    //初始化一个空的根
+    T->keyNum = 0;
+    T->parent = NULL;
+    for (int i = 0; i < M + 1; i++)
+    {
+        T->ptr[i] = NULL;
+    }
 }
 
 int main()
 {
-    mRootTree=new BTreeNode();
+    mRootTree = new BTreeNode();
     InitialBTree(mRootTree);
     int a[11] = {45, 24, 53, 90, 3, 12, 50, 61, 70, 100};
     for (int i = 0; i < 4; i++)
@@ -230,5 +276,8 @@ int main()
 
         insertData(a[i]);
     }
+    cout<<mRootTree;
+	cout<<"OK";
+
     return 0;
 }
