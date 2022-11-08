75e1f6b01db383950ac6ac25ed0443e91f9bedd3
zhangkangbin <784908058@qq.com>
2022-11-08 22:30:31 +0800

    quick

--- chapter8_sort/ExchangeSort.cpp
+++ chapter8_sort/ExchangeSort.cpp
@@ -2,7 +2,7 @@
  * @Author: zhangkangbin
  * @Date: 2022-09-15 14:37:53
  * @LastEditors: zhangkangbin
- * @LastEditTime: 2022-11-07 22:56:53
+ * @LastEditTime: 2022-11-08 22:17:45
  * @FilePath: \C_Study\chapter8_sort\ExchangeSort.cpp
  * 交换排序：
  * 1，冒泡
@@ -12,7 +12,10 @@
 #include <iostream>
 using namespace std;
 
-void printData(int list[],int length)
+#define MAXSIZE 8
+
+void printData(int list[], int length)
+
 {
     cout << " \n";
 
@@ -22,7 +25,7 @@ void printData(int list[],int length)
         cout << " " << list[i];
     }
 
-      cout << " \n";
+    cout << " \n";
 }
 
 /**
@@ -72,7 +75,7 @@ int getPartition(int list[], int low, int high)
 
     while (low < high)
     {
-        //high 开始
+        // high 开始
         while (low < high && list[high] >= partition)
         {
             high--;
@@ -80,11 +83,11 @@ int getPartition(int list[], int low, int high)
         //直到找到参考值小的。
         list[low] = list[high];
 
-         cout << "  \n--------high----------\n";
+      /*  cout << "  \n--------high----------\n";
 
-        printData(list,6);
-        
-         cout << "  \n----------low--------\n";
+       printData(list, MAXSIZE);
+
+        cout << "  \n----------low--------\n"; */
         while (low < high && list[low] <= partition)
         {
             low++;
@@ -92,17 +95,16 @@ int getPartition(int list[], int low, int high)
         //高低位交换位置。
         list[high] = list[low];
 
-        if(list[high] == list[low]){
-               cout << "  \n-----------<<"<<list[low]<<"-------\n";
+        if (list[high] == list[low])
+        {
+          //  cout << "  \n-----------<<" << list[low] << "-------\n";
         }
-
-        printData(list,6);
-
-       cout << "  \n------------------\n";
     }
     //最后一个值和参考值P交换位置
     list[low] = partition;
-
+    
+    printData(list, MAXSIZE);
+    cout << "  \n-------end-----------\n";
     return low;
 }
 
@@ -112,12 +114,12 @@ int getPartition(int list[], int low, int high)
  */
 void quickSort(int list[], int low, int high)
 {
-    //把数组分成两半。partition参考值下标
-    int partition = getPartition(list, low, high);
 
     if (low < high)
     {
-         
+        //把数组分成两半。partition参考值下标
+        int partition = getPartition(list, low, high);
+
         //递归
         quickSort(list, low, partition - 1); //左子表递归排序。（low,partition-1）
 
@@ -128,25 +130,24 @@ void quickSort(int list[], int low, int high)
 void testQuickSort()
 {
 
-    const int length = 6;
-    int listData[length] = {7, 8, 5, 6, 10,9};
+    //  int listData[MAXSIZE] = {5,3,9,8,4,2,1,7,6};
+    int listData[MAXSIZE] = {49, 38, 65, 97, 76, 13, 27, 49};
 
-    quickSort(listData, 0, (length - 1));
+    quickSort(listData, 0, (MAXSIZE - 1));
 
-    for (int i = 0; i < length; i++)
+/*     for (int i = 0; i < MAXSIZE; i++)
     {
 
         cout << " " << listData[i];
-    }
+    } */
 }
 
 int main()
 {
-   // bubbleSort();
-   
-   //快速排序
+    // bubbleSort();
+
+    //快速排序
     testQuickSort();
-    
-  
+
     return 0;
 }
